260abc228644b00503f55ad4f1da6fe5f898ce77
Bove <rdbv@gft.com>
2017-02-02 11:37:00 -0600

    Add array examples

--- java-fundamentals/io-stream/src/com/company/Main.java
+++ java-fundamentals/io-stream/src/com/company/Main.java
@@ -28,14 +28,64 @@ public class Main {
          * Reading chars 1 by 1
          */
 
-        System.out.println("**********  Reading chars 1 by 1  **********\n\n");
+        System.out.println("\n\n**********  Reading chars 1 by 1  **********\n\n");
 
-        int intVal;
         try (Reader reader = new FileReader("input.txt")) {
+            int intVal;
             while ((intVal = reader.read()) >= 0) {
                 char byteVal = (char) intVal;
                 System.out.println("Char value: " + byteVal);
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Reading an array of bytes
+         */
+
+        System.out.println("\n\n**********  Reading an array of bytes  **********\n\n");
+
+        try (InputStream inFile = new FileInputStream("input.txt")) {
+
+            int length;
+            byte[] byteBuff = new byte[10];
+            while ((length = inFile.read(byteBuff)) >= 0) {
+
+                for (int i = 0; i < length; i++) {
+                    byte byteVal = byteBuff[i];
+                    System.out.println("Byte value: " + byteVal);
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Reading an array of chars
+         */
+
+        System.out.println("\n\n**********  Reading an array of chars  **********\n\n");
+
+        try (Reader inFile = new FileReader("input.txt")) {
+
+            int length;
+            char[] byteBuff = new char[10];
+            while ((length = inFile.read(byteBuff)) >= 0) {
+
+                for (int i = 0; i < length; i++) {
+                    char byteVal = byteBuff[i];
+                    System.out.println("Char value: " + byteVal);
+                }
+
+            }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
