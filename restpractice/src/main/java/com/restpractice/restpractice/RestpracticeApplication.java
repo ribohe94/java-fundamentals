105c64ae216a71e2e307fc8f919518001686652c
Bove <ribohe94@gmail.com>
2017-11-26 20:07:51 -0600

    Create base REST project add gitignore properties

---
+++ restpractice/src/main/java/com/restpractice/restpractice/RestpracticeApplication.java
@@ -0,0 +1,12 @@
+package com.restpractice.restpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestpracticeApplication.class, args);
+	}
+}
